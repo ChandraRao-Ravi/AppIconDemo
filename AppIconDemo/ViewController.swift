4bafbf8f72a5f1a305029b56ce41c68fc4288ac2
Chandra Rao <chandra.rao@diaspark.com>
2017-12-09 16:24:59 +0530

    Initial commit

---
+++ AppIconDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppIconDemo
+//
+//  Created by Chandra Rao on 04/12/17.
+//  Copyright © 2017 Chandra Rao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
